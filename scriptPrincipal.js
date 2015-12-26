ea5fc0866ffba9ccad05f43ea79a5a8f636bddb3
Ovidio Arteaga <orvix0286@gmail.com>
2015-12-26 04:09:17 -0430

    Nueva version del formulario con nuevas fuentes

--- scriptPrincipal.js
+++ scriptPrincipal.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	//Objetos de configuracion con todas las opciones aplicadas
+	var idLista, idCanal, videoId;
+
 	var objetoConfiguracionPlaylistItems = {
 		part: "snippet,contentDetails",
 		playlistId: idLista,
@@ -9,7 +11,7 @@ $(document).ready(function(){
 
 	var objetoConfiguracionChannels = {
 		part: "snippet",
-		id: idCanal
+		id: idCanal,
 		fields: "items(snippet(title,description,thumbnails(medium)))"
 	}
 
@@ -18,4 +20,15 @@ $(document).ready(function(){
 		id: videoId,
 		fields: "items(snippet(publishedAt,title,description,thumbnails(medium)),contentDetails(duration,definition),statistics)"
 	}
+
+	$(".config").click(function(){
+		$(".overlay-container").fadeIn(function(){
+			$(".window-container.zoomout").addClass("window-container-visible");
+		});
+	});
+
+	$(".close, .cerrar").click(function(){
+		$(".overlay-container").fadeOut().end().find(".window-container").removeClass("window-container-visible");
+	});
+
 });
