8741ce6a1c0857c593868567ff22a8e4e0870352
Ovidio Arteaga <orvix0286@gmail.com>
2016-01-11 00:12:44 -0430

    comenzando a crear el sistema de busqueda

--- scriptBuscador.js
+++ scriptBuscador.js
@@ -1,11 +1,32 @@
 $(document).ready(function(){
 
- var funcionInicial = function(){
+
+    
+    var peticion = function(){
+
+        var objetoConfiguracion = {
+            part: "id,snippet",
+            type: "playlist", 
+            maxResults: "10",
+            q: $("#textoBusqueda").val()
+        };
+
+        var request = gapi.client.youtube.search.list(objetoConfiguracion);
+            request.then(function(response){
+                
+        });
+
+    };
+    
+
+    var funcionInicial = function(){
         //Inserto clases para modificar la pocicion de la caja de busqueda
         $("form p:first").addClass("resultadoBuscador");
         $("form h2:first").css("display", "none");
         $(".dame").addClass("dame-result");
 
+        gapi.client.setApiKey("AIzaSyARaWBizwChYj0ROHcQHaj23de5d2wj9NQ");
+        gapi.client.load("youtube", "v3").then(peticion);
     };
 
     //Accion para mostrar la informacion de la lista de reproduccion
